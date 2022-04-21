b730c5f00b506d6437042907f24f79972ca34cba
Michael J. Miller <commnerd@gmail.com>
2022-04-21 12:28:28 -0600

    Major update to configs

--- src/config.rs
+++ src/config.rs
@@ -1,31 +1,27 @@
-use config::Config as ImpConfig;
+use config::Config as DepConfig;
 use std::collections::HashMap;
 
-pub struct Config {
-    config: Box<ImpConfig>
-}
+pub struct Config(Option<DepConfig>);
 
 impl Default for Config {
     fn default() -> Self {
-        Config{
-            config: Box::new(ImpConfig::default()),
-        }
+        Config(None)
     }
 }
 
 impl Config {
-    pub fn init() -> Config {
-        let imp_config = ImpConfig::builder()
-            // Add in `./Settings.toml`
-            .add_source(config::File::with_name("examples/simple/Settings"))
+    pub fn init() -> Self {
+        Config(Some(DepConfig::builder()
             // Add in settings from the environment (with a prefix of APP)
             // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
-            .add_source(config::Environment::with_prefix("APP"))
+            .add_source(config::Environment::with_prefix("SW"))
+            // Add in `./Settings.toml`
+            .add_source(config::File::with_name("/etc/spiderweb/config").required(false))
             .build()
-            .unwrap();
-    
-        Config{
-            config: Box::new(imp_config),
-        }
+            .unwrap()))
+    }
+
+    pub fn get_int(&self, key: &str) {
+        self.get_int(key)
     }
 }
