ee4ac19a85f034b110e77263f17c57e56fcf4d0c
Michael J. Miller <commnerd@gmail.com>
2022-05-02 10:05:58 -0600

    Added resources endpoint

--- src/web/server.rs
+++ src/web/server.rs
@@ -5,7 +5,17 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/processes")]
+fn processes() -> &'static str {
+    "This will list processes"
+}
+
 pub fn serve(app: &App) {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![
+            index,
+            processes
+        ])
+        .launch();
 }
 
